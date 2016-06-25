e1e7df8e61805eeea3dd10569aa84ed940a854ac
songchen <280724396@qq.com>
2016-06-25 09:16:01 +0800

    绘制棋子
    ontouch and draw point

--- myview/src/main/java/com/example/myview/MyView.java
+++ myview/src/main/java/com/example/myview/MyView.java
@@ -2,17 +2,23 @@ package com.example.myview;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Song on 2016/6/21.
  */
-public class MyView extends View{
+public class MyView extends View {
 
-    private Bitmap mWith;
+    private Bitmap mWhite;
     private Bitmap mBlack;
 
     private Paint mPaint;
@@ -20,6 +26,14 @@ public class MyView extends View{
     private int mPanelWidth;
     private float mLineHeight;
     private int MAXLINE = 10;
+
+    private Point mPoint;
+    private List<Point> mWhiteArray = new ArrayList<>();
+    private List<Point> mBlackArray = new ArrayList<>();
+    //默认白棋先
+    private boolean mIsWhite = true;
+    //棋子比例
+    private float mScale = 3 * 1.0f / 4;
 
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -29,6 +43,8 @@ public class MyView extends View{
         mPaint.setAntiAlias(true);
         mPaint.setDither(true);
         mPaint.setStyle(Paint.Style.STROKE);
+        mWhite = BitmapFactory.decodeResource(getResources(), R.drawable.stone_w2);
+        mBlack = BitmapFactory.decodeResource(getResources(), R.drawable.stone_b1);
     }
 
     @Override
@@ -57,11 +73,40 @@ public class MyView extends View{
         super.onSizeChanged(w, h, oldw, oldh);
         mPanelWidth = w;
         mLineHeight = mPanelWidth * 1.0f / MAXLINE;
+
+        int width = (int) (mLineHeight * mScale);
+        mWhite = Bitmap.createScaledBitmap(mWhite, width, width, false);
+        mBlack = Bitmap.createScaledBitmap(mBlack, width, width, false);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        if (action == MotionEvent.ACTION_UP) {
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+            Point p = getPoint(x, y);
+            if (mWhiteArray.contains(p) || mBlackArray.contains(p))
+                return false;
+            if (mIsWhite) {
+                mWhiteArray.add(p);
+            } else {
+                mBlackArray.add(p);
+            }
+            invalidate();
+            mIsWhite = !mIsWhite;
+        }
+        return true;
+    }
+
+    private Point getPoint(int x, int y) {
+        return new Point((int) (x / mLineHeight), (int) (y / mLineHeight));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        //绘制棋盘
         int width = mPanelWidth;
         float lineHeight = mLineHeight;
 
@@ -75,6 +120,19 @@ public class MyView extends View{
 
             canvas.drawLine((float) ((0.5 + i) * lineHeight), startX,
                     (float) ((0.5 + i) * lineHeight), endX, mPaint);
+        }
+
+        //绘制棋子
+        for (int i = 0, n = mWhiteArray.size(); i < n; i++) {
+            Point p = mWhiteArray.get(i);
+            canvas.drawBitmap(mWhite, p.x * mLineHeight,
+                    p.y * mLineHeight, null);
+        }
+
+        for (int i = 0, n = mBlackArray.size(); i < n; i++) {
+            Point p = mBlackArray.get(i);
+            canvas.drawBitmap(mBlack, p.x * mLineHeight,
+                    p.y * mLineHeight, null);
         }
 
     }
