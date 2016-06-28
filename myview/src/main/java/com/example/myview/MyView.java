45b58b770cbd0a1f59799ca87b6c20c279b19829
songchen <280724396@qq.com>
2016-06-28 09:14:59 +0800

    完成逻辑

--- myview/src/main/java/com/example/myview/MyView.java
+++ myview/src/main/java/com/example/myview/MyView.java
@@ -9,6 +9,8 @@ import android.graphics.Point;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +20,7 @@ import java.util.List;
  */
 public class MyView extends View {
 
+    private static final int MAXPOINT = 5;
     private Bitmap mWhite;
     private Bitmap mBlack;
 
@@ -35,6 +38,11 @@ public class MyView extends View {
     //棋子比例
     private float mScale = 3 * 1.0f / 4;
 
+    private boolean isGameOver = false;
+    private boolean isWhiteWin = true;
+
+    private TextView displayTv;
+
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
@@ -45,6 +53,8 @@ public class MyView extends View {
         mPaint.setStyle(Paint.Style.STROKE);
         mWhite = BitmapFactory.decodeResource(getResources(), R.drawable.stone_w2);
         mBlack = BitmapFactory.decodeResource(getResources(), R.drawable.stone_b1);
+
+        displayTv = (TextView) findViewById(R.id.displaytv);
     }
 
     @Override
@@ -81,6 +91,9 @@ public class MyView extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        if (isGameOver)
+            return false;
+
         int action = event.getAction();
         if (action == MotionEvent.ACTION_UP) {
             int x = (int) event.getX();
@@ -90,8 +103,10 @@ public class MyView extends View {
                 return false;
             if (mIsWhite) {
                 mWhiteArray.add(p);
+                checkWin(p, mWhiteArray);
             } else {
                 mBlackArray.add(p);
+                if (checkWin(p, mBlackArray)) isWhiteWin = false;
             }
             invalidate();
             mIsWhite = !mIsWhite;
@@ -106,6 +121,150 @@ public class MyView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        drawPanel(canvas);
+        drawPoint(canvas);
+        if (isGameOver) {
+            if (isWhiteWin) {
+                Toast.makeText(getContext(), "白棋获胜", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getContext(), "黑棋获胜", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private boolean checkWin(Point p, List<Point> list) {
+        int x = p.x;
+        int y = p.y;
+        int count = 1;
+        //左遍历
+        for (int i = 1; i < MAXPOINT; i++) {
+            if (list.contains(new Point(x - i, y))) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        if (count == MAXPOINT) {
+            isGameOver = true;
+            return true;
+        }
+        //右遍历
+        for (int i = 1; i < MAXPOINT; i++) {
+            if (list.contains(new Point(x + i, y))) {
+                count++;
+            } else {
+                break;
+            }
+
+        }
+
+        if (count == MAXPOINT) {
+            isGameOver = true;
+            return true;
+        }
+        count = 1;
+        //上遍历
+        for (int i = 1; i < MAXPOINT; i++) {
+            if (list.contains(new Point(x, y + i))) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        if (count == MAXPOINT) {
+            isGameOver = true;
+            return true;
+        }
+        //下遍历
+        for (int i = 1; i < MAXPOINT; i++) {
+            if (list.contains(new Point(x, y - i))) {
+                count++;
+            } else {
+                break;
+            }
+
+        }
+
+        if (count == MAXPOINT) {
+            isGameOver = true;
+            return true;
+        }
+
+
+        //左斜上遍历/
+        count = 1;
+        for (int i = 1; i < MAXPOINT; i++) {
+            if (list.contains(new Point(x + i, y + i))) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        if (count == MAXPOINT) {
+            isGameOver = true;
+            return true;
+        }
+        //左斜下遍历
+        for (int i = 1; i < MAXPOINT; i++) {
+            if (list.contains(new Point(x - i, y - i))) {
+                count++;
+            } else {
+                break;
+            }
+
+        }
+
+        if (count == MAXPOINT) {
+            isGameOver = true;
+            return true;
+        }
+        //右斜上遍历
+        count = 1;
+        for (int i = 1; i < MAXPOINT; i++) {
+            if (list.contains(new Point(x - i, y + i))) {
+                count++;
+            } else {
+                break;
+            }
+        }
+        if (count == MAXPOINT) {
+            isGameOver = true;
+            return true;
+        }
+        //右斜下遍历
+        for (int i = 1; i < MAXPOINT; i++) {
+            if (list.contains(new Point(x + i, y - i))) {
+                count++;
+            } else {
+                break;
+            }
+
+        }
+
+        if (count == MAXPOINT) {
+            isGameOver = true;
+            return true;
+        }
+
+        return false;
+    }
+
+    private void drawPoint(Canvas canvas) {
+        //绘制棋子
+        for (int i = 0, n = mWhiteArray.size(); i < n; i++) {
+            Point p = mWhiteArray.get(i);
+            canvas.drawBitmap(mWhite, p.x * mLineHeight,
+                    p.y * mLineHeight, null);
+        }
+
+        for (int i = 0, n = mBlackArray.size(); i < n; i++) {
+            Point p = mBlackArray.get(i);
+            canvas.drawBitmap(mBlack, p.x * mLineHeight,
+                    p.y * mLineHeight, null);
+        }
+    }
+
+    private void drawPanel(Canvas canvas) {
         //绘制棋盘
         int width = mPanelWidth;
         float lineHeight = mLineHeight;
@@ -121,19 +280,7 @@ public class MyView extends View {
             canvas.drawLine((float) ((0.5 + i) * lineHeight), startX,
                     (float) ((0.5 + i) * lineHeight), endX, mPaint);
         }
-
-        //绘制棋子
-        for (int i = 0, n = mWhiteArray.size(); i < n; i++) {
-            Point p = mWhiteArray.get(i);
-            canvas.drawBitmap(mWhite, p.x * mLineHeight,
-                    p.y * mLineHeight, null);
-        }
-
-        for (int i = 0, n = mBlackArray.size(); i < n; i++) {
-            Point p = mBlackArray.get(i);
-            canvas.drawBitmap(mBlack, p.x * mLineHeight,
-                    p.y * mLineHeight, null);
-        }
-
     }
+
+    
 }
